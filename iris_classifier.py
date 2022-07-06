0d5725d88c6b822a6877eb9e5fc5806e55a6cf34
Alexa201228 <aleksandra.grid2012@gmail.com>
2022-07-06 22:01:21 +0300

    Analizing k_means

--- iris_classifier.py
+++ iris_classifier.py
@@ -44,3 +44,15 @@ clf = KNeighborsClassifier(n_neighbors=3)
 clf.fit(x_train, Y_train)
 print(f'Predictions on test set {clf.predict(x_test)}')
 print(f'Predictions accuracy {clf.score(x_test, Y_test)}')
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbours, ax in zip([1, 3, 9], axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbours).fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title(f'Neighbors count: {n_neighbours}')
+    ax.set_xlabel('Tag 0')
+    ax.set_ylabel('Tag 1')
+axes[0].legend(loc=3)
+plt.show()
