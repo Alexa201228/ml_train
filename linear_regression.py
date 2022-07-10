cfa9cdd3bff6f01babcbf6200713dd2d351764db
Alexa201228 <aleksandra.grid2012@gmail.com>
2022-07-10 20:15:24 +0300

    linear models logistic regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,8 @@
 import mglearn.datasets
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 X, y = mglearn.datasets.make_wave(n_samples=60)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
@@ -13,3 +15,19 @@ print("lr.intercept_: {}".format(lr.intercept_))
 print(f'Accuracy on training set {lr.score(X_train, y_train)}')
 print(f'Accuracy on test set {lr.score(X_test, y_test)}')
 
+# Logistic Regression
+
+X1, y1 = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X1, y1)
+    mglearn.plots.plot_2d_separator(clf, X1, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X1[:, 0], X1[:, 1], y1, ax=ax)
+    ax.set_title(f'{clf.__class__.__name__}')
+    ax.set_xlabel('Tag 0')
+    ax.set_ylabel('Tag 1')
+
+axes[0].legend()
+plt.show()
